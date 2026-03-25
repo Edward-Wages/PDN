2a33d6da86526de724f8405c27b11bca09c2fbde
Edward Wages <edwardwages27@gmail.com>
2026-03-25 12:57:20 -0500

    Need to redo convolution kernel

--- Project_4_Problems/Problem_3/kernel.cu
+++ Project_4_Problems/Problem_3/kernel.cu
@@ -1,31 +1,39 @@
 
 __global__
-void convolution_kernel(unsigned int* d_mat_input, unsigned int* d_mat_kernel, unsigned int* d_mat_output, unsigned int n_row, unsigned int n_col) {
-
-    // Calculate thread index
-    unsigned int row = blockDim.y * blockIdx.y + threadIdx.y;
-    unsigned int col = blockDim.x * blockIdx.x + threadIdx.x;
-
-    if(row < n_row && col < n_col)
-    {
-        unsigned int sum = 0;
-
-        for(int i = 0; i < 5; i ++)
+void convolution_kernel(unsigned int* d_mat_input, unsigned int* d_mat_kernel, unsigned int* d_mat_output, unsigned int n_row, unsigned int n_col) 
+{
+    //Redo - math was bad
+    /* See this example of doing it serially:
+    // Performing convolution
+	// Take a look at slides about the blurring example - Blur size = 2, it's a 5x5 filter
+    for(int i=0; i<n_row; i++)
+        for (int j=0; j<n_col; j++)
         {
-            for(int j = 0; j < 5; j++)
-            {
-                int r = (int)row + i - 2; //Centering the kernel on the current pixel
-                int c = (int)col + j - 2;
+            int sum_val = 0;
 
-                //boundary check
-                if(r >= 0 && r < n_row && c >= 0 && c < n_col)
+            for (int blurRow = -BLUR_SIZE; blurRow < BLUR_SIZE +1; ++blurRow)
+            {
+                for(int blurCol = -BLUR_SIZE; blurCol < BLUR_SIZE+1; ++blurCol)
                 {
-                    unsigned int pixel_value = d_mat_input[r * n_col + c];
-                    unsigned int kernel_value = d_mat_kernel[i * 5 + j];
-                    sum += pixel_value * kernel_value;
+                    int curRow = i + blurRow;
+                    int curCol = j + blurCol;
+
+                    int i_row = blurRow + BLUR_SIZE;
+                    int i_col = blurCol + BLUR_SIZE;
+
+                    if( curRow > -1 && curRow < n_row && curCol > -1 && curCol < n_col)
+                    {
+                        sum_val += inputMatrix_h[curRow*n_col + curCol]*filterMatrix_h[i_row*5 + i_col]; 
+                    }
                 }
             }
+
+            outputMatrix_h[i*n_col+j] = sum_val;
+
         }
-        d_mat_output[row * n_col + col] = sum;
-    }
+    */
+
+
+
+
 }
