0908a13fe78d02b22994b005008ed0478de76b12
Edward Wages <edwardwages27@gmail.com>
2026-04-16 00:44:21 -0500

    Project 6 init

---
+++ Wages_Edward_Project6/Problem_1/find_longest_words_MRJob.py
@@ -0,0 +1,12 @@
+#Your assignment is to write a MapReduce program using MRJob to find the longest word
+#starting with each letter from a to z. If multiple words have the same length, they should be all
+#listed. A part of expected result is shown below:
+
+#"r" ["recommendations", "representations"]
+#"s" ["superciliousness"]
+#"t" ["thoughtlessness"]
+#"g" ["gentlemanlike", "gratification"]
+#"h" ["hertfordshire", "healthfulness"]
+
+# Program is ran like so: python find_longest_words_MRJob.py 1342-0.txt
+
